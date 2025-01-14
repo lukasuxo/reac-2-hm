069a46bf630e2353d86146971e6d220919764c78
lukasuxo <lukasukhiashvili06@gmail.com>
2025-01-15 02:18:38 +0400

    1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>suxo</title>
+        </head>
+        <body></body>
+      </html>
+    </>
+  );
+}
+
+export default App;
